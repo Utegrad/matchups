8ca687d0fc264d2c14fcb1a20850c9319a7d566a
Matthew Larsen <utegrad@gmail.com>
2015-05-26 23:50:14 -0600

    adding models and migrations

--- brackets/models.py
+++ brackets/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+def img_upload_dir_path(instance, filename):
+    """files to be saved to MEDIA_ROOT/uploads/%Y_%m_%d_filename"""
+    return 'uploads/%Y_%m_%d_{0}'.format(filename)
+
+
 # Create your models here.
+class FIFATeam(models.Model):
+    team_name = models.CharField(max_length=100, help_text="FIFA Team Name", unique=True)
+    team_avatar = models.ImageField(upload_to=img_upload_dir_path, )
+
+class Match(models.Model):
+    match_date = models.DateTimeField('when match played', null=True, blank=True, default='')
+    contestant_one_score = models.IntegerField(null=True, blank=True, default=0,)
+    contestant_two_score = models.IntegerField(null=True, blank=True, default=0,)
+
--- matchups/urls.py
+++ matchups/urls.py
@@ -17,6 +17,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.views.generic.base import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
